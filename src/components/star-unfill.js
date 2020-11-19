9a416f51a055d2c5d3480815b3effa891cebb56a
wascoyur <mytem20007@gmail.com>
2020-11-19 17:43:15 +0500

    Селекоры специй вынесены в компанент

---
+++ src/components/star-unfill.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import icon from "../assets/images/SM _ Star_filled.png"
+const StarFill = ()=>{
+  return(
+    <div>
+      <img src= {icon}/>
+    </div>
+  )
+}
+export default StarFill
