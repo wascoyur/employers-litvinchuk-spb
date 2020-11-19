5327d501b1035e4a98817dc3b4962de8ca5cb53c
wascoyur <mytem20007@gmail.com>
2020-11-19 20:24:26 +0500

    fix: кнопки корзины реализованы

--- src/components/card-details.js
+++ src/components/card-details.js
@@ -5,7 +5,7 @@ const CardDetail = ()=>{
   const [cur, addCount] = useState(1);
   
   return(
-      <div className="m-1 md:h-16 w-1/2 ">
+      <div className="m-1  w-1/2 ">
         <div id="short-description " className="p-4" >
           <div id="header"className="flex justify-between">
             <h3 id="title" className="text-xl font-extrabold">Ветчина и грибы</h3>
@@ -29,13 +29,13 @@ const CardDetail = ()=>{
         <div id="description" className="p-5 bg-gray-900 text-white">
           <div id="title" className="">Описание</div>
           <div id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore , sed do eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore , sed do eiusmod tempor incididunt ut labore</div>
-          <div id="weight" className="bg-gray-600">425gr</div>
+          <div id="weight" className="m-3 text-gray-600">425gr</div>
           <div id ="counter-wrapper" className="flex justify-between text-2xl">
-            <button className="btn w-30"
+            <button className="btn w-8 m-4 bg-white text-black"
               onClick={()=>{addCount(cur +1)}}
             >+</button>
-              <div id="count">{ cur }</div>
-            <button className="btn"
+              <div id="count" className="m-4">{ cur }</div>
+            <button className="btn w-8 m-4 bg-white text-black"
               onClick={() =>{
                 if (cur<=1)return cur;
                 addCount(cur -1)}}
